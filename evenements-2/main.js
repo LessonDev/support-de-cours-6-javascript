0d447419f7af7ff106ec1b9ea2b14616bf30862d
LessonDev <fl.lesson.dev@gmail.com>
2020-11-19 21:27:39 +0100

    correction/support de cours digistart javascript n°6

---
+++ evenements-2/main.js
@@ -0,0 +1,29 @@
+/* LES ÉVÉNEMENTS */
+console.log('Bienvenue dans la console javascript');
+console.log("Vous êtes dans l'exercice 2 sur les événements");
+
+// ------ TODO 1 ------
+// Créez une variable récupérant l'élément button du HTML.
+
+
+var myButton = document.getElementById("idButton") ;
+
+
+// ------ TODO 2 ------
+// Créer la fonction qui sera appelée dans le TODO3.
+// Elle devra afficher dans la console "Le bouton a été cliqué",
+// ainsi qu'une alerte "Click !".
+
+function  myFunctionClickMy(){
+
+   console.log("Le bouton a été cliqué");
+   alert("Click !");
+}
+
+// ------ TODO 3 ------
+// Attacher un événement "click" sur le bouton, et appeler la fonction lors du clic.
+
+myButton.addEventListener("click", myFunctionClickMy);
+
+
+
